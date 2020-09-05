02a29c99b054bc88180fa237ed4f1c7248e55810
Miguel Tapia Montecinos <a2019118017@estudiantes.upsa.edu.bo>
2020-09-05 13:17:57 -0400

    Se creò el UnitTest de APIStudent

---
+++ APIStudentSecurity/StudentSecurity.Tests/Controllers/UnitTestAPIStudent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using APIStudentSecurity.Controllers;
+using APIStudentSecurity.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace StudentSecurity.Tests.Controllers
+{   [TestClass]
+    public class UnitTestAPIStudent
+    {
+        [TestMethod]
+        public void TestGetStudent()
+        {
+            //Arrange
+            StudentsController studentsController = new StudentsController(); //objeto
+
+            //Act
+            var listaEstudiantes = studentsController.GetStudents();
+
+            //Assert
+            Assert.IsNotNull(listaEstudiantes);
+
+        }
+    }
+}
